270545542f23eb691950b359fe4de6e067db248d
Darina Daly <65533948+darinadaly@users.noreply.github.com>
2020-07-31 11:28:30 -0700

    Add files via upload

---
+++ HD23642.py
@@ -0,0 +1,389 @@
+# example notebook for HD 23642
+
+#%matplotlib inline
+#%run notebook_setup
+"""isort:skip_file"""
+
+get_ipython().magic('config InlineBackend.figure_format = "retina"')
+
+import os
+import logging
+import warnings
+
+import matplotlib.pyplot as plt
+
+# Don't use the schmantzy progress bar
+os.environ["EXOPLANET_NO_AUTO_PBAR"] = "true"
+
+# Remove when Theano is updated
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+# Remove when arviz is updated
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
+logger = logging.getLogger("theano.gof.compilelock")
+logger.setLevel(logging.ERROR)
+logger = logging.getLogger("exoplanet")
+logger.setLevel(logging.DEBUG)
+
+
+plt.style.use("default")
+plt.rcParams["savefig.dpi"] = 100
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["font.size"] = 16
+plt.rcParams["font.family"] = "sans-serif"
+plt.rcParams["font.sans-serif"] = ["Liberation Sans"]
+plt.rcParams["font.cursive"] = ["Liberation Sans"]
+plt.rcParams["mathtext.fontset"] = "custom"
+
+
+
+
+# DATA ACCESS
+lit_period = 2.46113408
+lit_t0 = 119.522070 + 2457000 - 2454833
+# Prior on the flux ratio for Kepler
+lit_flux_ratio = (0.354, 0.035)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import lightkurve as lk
+
+#light curve
+tpf = lk.search_targetpixelfile("EPIC 211082420").download()
+lc = tpf.to_lightcurve(aperture_mask="all")
+lc = lc.remove_nans().normalize()
+
+hdr = tpf.hdu[1].header
+texp = hdr["FRAMETIM"] * hdr["NUM_FRM"]
+texp /= 60.0 * 60.0 * 24.0
+
+x = np.ascontiguousarray(lc.time, dtype=np.float64)
+y = np.ascontiguousarray(lc.flux, dtype=np.float64)
+mu = np.median(y)
+y = (y / mu - 1) * 1e3
+
+plt.plot((x - lit_t0 + 0.5 * lit_period) % lit_period - 0.5 * lit_period, y, ".k")
+plt.xlim(-0.5 * lit_period, 0.5 * lit_period)
+plt.xlabel("time since primary eclipse [days]")
+_ = plt.ylabel("relative flux [ppt]")
+plt.show()
+
+#RV data
+ref1 = 2453000
+ref2 = 2400000
+rvs = np.array(
+    [
+        # https://arxiv.org/abs/astro-ph/0403444
+        (39.41273 + ref1, -85.0, 134.5),
+        (39.45356 + ref1, -88.0, 139.0),
+        (39.50548 + ref1, -91.0, 143.0),
+        (43.25049 + ref1, 105.5, -136.0),
+        (46.25318 + ref1, 29.5, -24.5),
+        # https://ui.adsabs.harvard.edu/abs/2007A%26A...463..579G/abstract
+        (52629.6190 + ref2, 88.8, -127.0),
+        (52630.6098 + ref2, -48.0, 68.0),
+        (52631.6089 + ref2, -9.5, 13.1),
+        (52632.6024 + ref2, 63.6, -90.9),
+        (52633.6162 + ref2, -94.5, 135.0),
+        (52636.6055 + ref2, 10.3, -13.9),
+        (52983.6570 + ref2, 18.1, -25.1),
+        (52987.6453 + ref2, -80.6, 114.5),
+        (52993.6322 + ref2, 49.0, -70.7),
+        (53224.9338 + ref2, 39.0, -55.7),
+        (53229.9384 + ref2, 57.2, -82.0),
+    ]
+)
+rvs[:, 0] -= 2454833
+rvs = rvs[np.argsort(rvs[:, 0])]
+
+x_rv = np.ascontiguousarray(rvs[:, 0], dtype=np.float64)
+y1_rv = np.ascontiguousarray(rvs[:, 1], dtype=np.float64)
+y2_rv = np.ascontiguousarray(rvs[:, 2], dtype=np.float64)
+
+fold = (rvs[:, 0] - lit_t0 + 0.5 * lit_period) % lit_period - 0.5 * lit_period
+plt.plot(fold, rvs[:, 1], ".", label="primary")
+plt.plot(fold, rvs[:, 2], ".", label="secondary")
+plt.legend(fontsize=10)
+plt.xlim(-0.5 * lit_period, 0.5 * lit_period)
+plt.ylabel("radial velocity [km / s]")
+_ = plt.xlabel("time since primary eclipse [days]")
+
+
+
+
+# PROBABILISTIC MODEL
+import multiprocessing as mp
+mp.set_start_method("fork")
+
+import theano
+theano.config.gcc.cxxflags = "-Wno-c++11-narrowing" 
+
+import pymc3 as pm
+import theano.tensor as tt
+
+import exoplanet as xo
+
+
+def build_model(mask):
+
+    with pm.Model() as model:
+
+        # Systemic parameters
+        mean_lc = pm.Normal("mean_lc", mu=0.0, sd=5.0)
+        mean_rv = pm.Normal("mean_rv", mu=0.0, sd=50.0)
+        u1 = xo.QuadLimbDark("u1")
+        u2 = xo.QuadLimbDark("u2")
+
+        # Parameters describing the primary
+        M1 = pm.Lognormal("M1", mu=0.0, sigma=10.0)
+        R1 = pm.Lognormal("R1", mu=0.0, sigma=10.0)
+
+        # Secondary ratios
+        k = pm.Lognormal("k", mu=0.0, sigma=10.0)  # radius ratio
+        q = pm.Lognormal("q", mu=0.0, sigma=10.0)  # mass ratio
+        s = pm.Lognormal("s", mu=np.log(0.5), sigma=10.0)  # surface brightness ratio
+
+        # Prior on flux ratio
+        pm.Normal(
+            "flux_prior",
+            mu=lit_flux_ratio[0],
+            sigma=lit_flux_ratio[1],
+            observed=k ** 2 * s,
+        )
+
+        # Parameters describing the orbit
+        b = xo.ImpactParameter("b", ror=k, testval=1.5)
+        period = pm.Lognormal("period", mu=np.log(lit_period), sigma=1.0)
+        t0 = pm.Normal("t0", mu=lit_t0, sigma=1.0)
+
+        # Parameters describing the eccentricity: ecs = [e * cos(w), e * sin(w)]
+        ecs = xo.UnitDisk("ecs", testval=np.array([1e-5, 0.0]))
+        ecc = pm.Deterministic("ecc", tt.sqrt(tt.sum(ecs ** 2)))
+        omega = pm.Deterministic("omega", tt.arctan2(ecs[1], ecs[0]))
+
+        # Build the orbit
+        R2 = pm.Deterministic("R2", k * R1)
+        M2 = pm.Deterministic("M2", q * M1)
+        orbit = xo.orbits.KeplerianOrbit(
+            period=period,
+            t0=t0,
+            ecc=ecc,
+            omega=omega,
+            b=b,
+            r_star=R1,
+            m_star=M1,
+            m_planet=M2,
+        )
+
+        # Track some other orbital elements
+        pm.Deterministic("incl", orbit.incl)
+        pm.Deterministic("a", orbit.a)
+
+        # Noise model for the light curve
+        sigma_lc = pm.InverseGamma(
+            "sigma_lc", testval=1.0, **xo.estimate_inverse_gamma_parameters(0.1, 2.0)
+        )
+        S_tot_lc = pm.InverseGamma(
+            "S_tot_lc", testval=2.5, **xo.estimate_inverse_gamma_parameters(1.0, 5.0)
+        )
+        ell_lc = pm.InverseGamma(
+            "ell_lc", testval=2.0, **xo.estimate_inverse_gamma_parameters(1.0, 5.0)
+        )
+        kernel_lc = xo.gp.terms.SHOTerm(
+            S_tot=S_tot_lc, w0=2 * np.pi / ell_lc, Q=1.0 / 3
+        )
+
+        # Noise model for the radial velocities
+        sigma_rv1 = pm.InverseGamma(
+            "sigma_rv1", testval=1.0, **xo.estimate_inverse_gamma_parameters(0.5, 5.0)
+        )
+        sigma_rv2 = pm.InverseGamma(
+            "sigma_rv2", testval=1.0, **xo.estimate_inverse_gamma_parameters(0.5, 5.0)
+        )
+        S_tot_rv = pm.InverseGamma(
+            "S_tot_rv", testval=2.5, **xo.estimate_inverse_gamma_parameters(1.0, 5.0)
+        )
+        ell_rv = pm.InverseGamma(
+            "ell_rv", testval=2.0, **xo.estimate_inverse_gamma_parameters(1.0, 5.0)
+        )
+        kernel_rv = xo.gp.terms.SHOTerm(
+            S_tot=S_tot_rv, w0=2 * np.pi / ell_rv, Q=1.0 / 3
+        )
+
+        # Set up the light curve model
+        lc = xo.SecondaryEclipseLightCurve(u1, u2, s)
+
+        def model_lc(t):
+            return (
+                mean_lc
+                + 1e3 * lc.get_light_curve(orbit=orbit, r=R2, t=t, texp=texp)[:, 0]
+            )
+
+        # Condition the light curve model on the data
+        gp_lc = xo.gp.GP(
+            kernel_lc, x[mask], tt.zeros(mask.sum()) ** 2 + sigma_lc ** 2, mean=model_lc
+        )
+        gp_lc.marginal("obs_lc", observed=y[mask])
+
+        # Set up the radial velocity model
+        def model_rv1(t):
+            return mean_rv + 1e-3 * orbit.get_radial_velocity(t)
+
+        def model_rv2(t):
+            return mean_rv - 1e-3 * orbit.get_radial_velocity(t) / q
+
+        # Condition the radial velocity model on the data
+        gp_rv1 = xo.gp.GP(
+            kernel_rv, x_rv, tt.zeros(len(x_rv)) ** 2 + sigma_rv1 ** 2, mean=model_rv1
+        )
+        gp_rv1.marginal("obs_rv1", observed=y1_rv)
+        gp_rv2 = xo.gp.GP(
+            kernel_rv, x_rv, tt.zeros(len(x_rv)) ** 2 + sigma_rv2 ** 2, mean=model_rv2
+        )
+        gp_rv2.marginal("obs_rv2", observed=y2_rv)
+
+        # Optimize the logp
+        map_soln = model.test_point
+
+        # First the RV parameters
+        map_soln = xo.optimize(map_soln, [mean_rv, q])
+        map_soln = xo.optimize(
+            map_soln, [mean_rv, sigma_rv1, sigma_rv2, S_tot_rv, ell_rv]
+        )
+
+        # Then the LC parameters
+        map_soln = xo.optimize(map_soln, [mean_lc, R1, k, s, b])
+        map_soln = xo.optimize(map_soln, [mean_lc, R1, k, s, b, u1, u2])
+        map_soln = xo.optimize(map_soln, [mean_lc, sigma_lc, S_tot_lc, ell_lc])
+        map_soln = xo.optimize(map_soln, [t0, period])
+
+        # Then all the parameters together
+        map_soln = xo.optimize(map_soln)
+
+        model.gp_lc = gp_lc
+        model.model_lc = model_lc
+        model.gp_rv1 = gp_rv1
+        model.model_rv1 = model_rv1
+        model.gp_rv2 = gp_rv2
+        model.model_rv2 = model_rv2
+
+        model.x = x[mask]
+        model.y = y[mask]
+
+    return model, map_soln
+
+
+def sigma_clip():
+    mask = np.ones(len(x), dtype=bool)
+    num = len(mask)
+
+    for i in range(10):
+        model, map_soln = build_model(mask)
+
+        with model:
+            mdl = xo.eval_in_model(
+                model.model_lc(x[mask]) + model.gp_lc.predict(), map_soln
+            )
+
+        resid = y[mask] - mdl
+        sigma = np.sqrt(np.median((resid - np.median(resid)) ** 2))
+        mask[mask] = np.abs(resid - np.median(resid)) < 7 * sigma
+        print("Sigma clipped {0} light curve points".format(num - mask.sum()))
+        if num == mask.sum():
+            break
+        num = mask.sum()
+
+    return model, map_soln
+
+
+model, map_soln = sigma_clip()
+
+
+
+period = map_soln["period"]
+t0 = map_soln["t0"]
+mean = map_soln["mean_rv"]
+
+x_fold = (x_rv - t0 + 0.5 * period) % period - 0.5 * period
+plt.plot(fold, y1_rv - mean, ".", label="primary")
+plt.plot(fold, y2_rv - mean, ".", label="secondary")
+
+x_phase = np.linspace(-0.5 * period, 0.5 * period, 500)
+with model:
+    y1_mod, y2_mod = xo.eval_in_model(
+        [model.model_rv1(x_phase + t0), model.model_rv2(x_phase + t0)], map_soln
+    )
+plt.plot(x_phase, y1_mod - mean, "C0")
+plt.plot(x_phase, y2_mod - mean, "C1")
+
+plt.legend(fontsize=10)
+plt.xlim(-0.5 * period, 0.5 * period)
+plt.ylabel("radial velocity [km / s]")
+plt.xlabel("time since primary eclipse [days]")
+_ = plt.title("HD 23642; map model", fontsize=14)
+plt.show()
+
+
+with model:
+    gp_pred = xo.eval_in_model(model.gp_lc.predict(), map_soln) + map_soln["mean_lc"]
+    lc = xo.eval_in_model(model.model_lc(model.x), map_soln) - map_soln["mean_lc"]
+
+fig, (ax1, ax2) = plt.subplots(2, sharex=True, figsize=(12, 7))
+
+ax1.plot(model.x, model.y, "k.", alpha=0.2)
+ax1.plot(model.x, gp_pred, color="C1", lw=1)
+
+ax2.plot(model.x, model.y - gp_pred, "k.", alpha=0.2)
+ax2.plot(model.x, lc, color="C2", lw=1)
+ax2.set_xlim(model.x.min(), model.x.max())
+
+ax1.set_ylabel("raw flux [ppt]")
+ax2.set_ylabel("de-trended flux [ppt]")
+ax2.set_xlabel("time [KBJD]")
+ax1.set_title("HD 23642; map model", fontsize=14)
+
+fig.subplots_adjust(hspace=0.05)
+
+fig, ax1 = plt.subplots(1, figsize=(12, 3.5))
+
+x_fold = (model.x - map_soln["t0"]) % map_soln["period"] / map_soln["period"]
+inds = np.argsort(x_fold)
+
+ax1.plot(x_fold[inds], model.y[inds] - gp_pred[inds], "k.", alpha=0.2)
+ax1.plot(x_fold[inds] - 1, model.y[inds] - gp_pred[inds], "k.", alpha=0.2)
+ax2.plot(x_fold[inds], model.y[inds] - gp_pred[inds], "k.", alpha=0.2, label="data!")
+ax2.plot(x_fold[inds] - 1, model.y[inds] - gp_pred, "k.", alpha=0.2)
+
+yval = model.y[inds] - gp_pred
+bins = np.linspace(0, 1, 75)
+num, _ = np.histogram(x_fold[inds], bins, weights=yval)
+denom, _ = np.histogram(x_fold[inds], bins)
+ax2.plot(0.5 * (bins[:-1] + bins[1:]) - 1, num / denom, ".w")
+
+args = dict(lw=1)
+
+ax1.plot(x_fold[inds], lc[inds], "C2", **args)
+ax1.plot(x_fold[inds] - 1, lc[inds], "C2", **args)
+
+ax1.set_xlim(-1, 1)
+ax1.set_ylabel("de-trended flux [ppt]")
+ax1.set_xlabel("phase")
+_ = ax1.set_title("HD 23642; map model", fontsize=14)
+
+
+
+
+# # MCMC
+# np.random.seed(23642)
+# with model:
+#     trace = xo.sample(
+#         tune=3500,
+#         draws=3000,
+#         start=map_soln,
+#         chains=4,
+#         initial_accept=0.8,
+#         target_accept=0.95,
+#     )
